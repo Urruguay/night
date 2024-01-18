64c84967f1bf0e8b83c791ef87eae98ea9a1551a
Urruguay <vikuliashurduk@gmail.com>
2024-01-18 10:55:44 +0200

    new

---
+++ 18.01.24/Point.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _18._01._24
+{
+    internal class Point
+    {
+        /// <summary>
+        /// Поля с координатами точек
+        /// </summary>
+        public double x;
+        public double y;
+
+        /// <summary>
+        /// Cтатические поля, опредеяющие количество точек 
+        /// 1-й, 2-й, 3-й, 4-й координатной четверти, а также общее количество точек
+        /// </summary>
+        private static int quarter1;
+        private static int quarter2;
+        private static int quarter3;
+        private static int quarter4;
+        private static int totalCount;
+
+        /// <summary>
+        /// Конструктор с параметрами, принимающий координаты точки
+        /// </summary>
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+
+            //// Определение четверти и обновление счетчиков
+            UpdateQuarterCount();
+            totalCount++;
+        }
+
+        /// <summary>
+        /// Метод для вывода координат точки
+        /// </summary>
+        public void GetPoint()
+        {
+            WriteLine($"Coordinates: ({x}, {y})");
+        }
+
+        /// <summary>
+        /// Статический метод, выводящий количество точек, 
+        /// расположенных в каждой координатной четверти, и общее количество точек
+        /// </summary>
+        public static void DisplayPointStatistics()
+        {
+            WriteLine($"Number of points in Quadrant 1: {quarter1}");
+            WriteLine($"Number of points in Quadrant 2: {quarter2}");
+            WriteLine($"Number of points in Quadrant 3: {quarter3}");
+            WriteLine($"Number of points in Quadrant 4: {quarter4}");
+            WriteLine($"Total number of points: {totalCount}");
+        }
+
+        /// <summary>
+        /// Приватный метод для определения четверти и обновления счетчиков
+        /// </summary>
+        private void UpdateQuarterCount()
+        {
+            if (x > 0 && y > 0)
+            {
+                quarter1++;
+            }
+            else if (x < 0 && y > 0)
+            {
+                quarter2++;
+            }
+            else if (x < 0 && y < 0)
+            {
+                quarter3++;
+            }
+            else if (x > 0 && y < 0)
+            {
+                quarter4++;
+            }
+        }
+    }
+}
--- 18.01.24/Program.cs
+++ 18.01.24/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace _18._01._24
 {
@@ -10,6 +12,30 @@ namespace _18._01._24
     {
         static void Main(string[] args)
         {
+            //// Пример использования класса Point
+            //Point point1 = new Point(2.0, 3.0);
+            //Point point2 = new Point(-1.0, 4.0);
+            //Point point3 = new Point(-2.0, -2.0);
+
+            //point1.GetPoint();
+            //point2.GetPoint();
+            //point3.GetPoint();
+
+            //Point.DisplayPointStatistics();
+
+            Point[] points = new Point[5];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Write("x = ");
+                double x = double.Parse(ReadLine());
+                Write("y = ");
+                double y = double.Parse(ReadLine());
+                points[i] = new Point(x, y);
+            }
+            
+            Point.DisplayPointStatistics();
+
+            ReadLine();
         }
     }
 }
