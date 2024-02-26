2a7df7f22ec8c015fbf475dbe5d10f41542b43b2
Urruguay <122653888+Urruguay@users.noreply.github.com>
2024-02-26 09:47:22 +0200

    Add files via upload
    add

---
+++ 26.02.24/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26._02._24
+{
+    class Square:Figure
+    {
+        private int side1;
+
+        public Square(string name):base(name) { }
+        public Square(int side1, string name) : base(name)
+        {
+            this.side1 = side1;
+        }
+        public int Side1 { get => side1; set => side1 = value; }
+        public override double Area()
+        {
+            return side1 * side1;
+        }
+        public override double Perimeter()
+        {
+            return Math.Pow(side1, 2) * 2 ;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"{side1}");
+        }
+    }
+}
