532ed0185dc9ce307c4312d29bd4e23cd6c8b248
Urruguay <vikuliashurduk@gmail.com>
2024-02-28 12:22:29 +0200

    add

---
+++ 19.02.24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _19._02._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите формат документа (XML, TXT, DOC):");
+            string format = ReadLine();
+
+            AbstractHandler handler = null;
+
+            switch (format.ToUpper())
+            {
+                case "XML":
+                    handler = new XMLHandler();
+                    break;
+                case "TXT":
+                    handler = new TXTHandler();
+                    break;
+                case "DOC":
+                    handler = new DOCHandler();
+                    break;
+                default:
+                    WriteLine("Неподдерживаемый формат документа.");
+                    return;
+            }
+
+            handler.Open();
+            handler.Create();
+            handler.Change();
+            handler.Save();
+
+            Read();
+        }
+    }
+}
